7cfac5d40dc53f8531f01376a5ab1e87bb637158
nickp <nickpinchuk91@gmail.com>
2022-12-15 17:26:08 +0200

    Red text

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello People");
         System.out.println("args = " + args);
         System.out.println("Test number two");
     }
