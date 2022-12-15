da2eac2f0ccfe9e60d21b720c1e46268073671d0
nickpinchuk <nickpinchuk91@gmail.com>
2022-12-16 00:56:58 +0200

    first commit two task

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello People");
+        System.out.print("Hello");
         System.out.println("args =  + args");
         System.out.println("Hello world!");
         System.out.println("args = " + args);
