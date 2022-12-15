6296b37ed97842af89f21e0d5e51834c476c8c8b
nickpinchuk <nickpinchuk91@gmail.com>
2022-12-16 01:14:49 +0200

    confl

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello wooooorl");
+        System.out.print("Hello People ");
         System.out.println("args =  + args");
         System.out.println("Test one");
         System.out.println("Test number two");
