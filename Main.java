3eba89da820f302e5fcb2b0f8a5660ee5036e79f
nickp <nickpinchuk91@gmail.com>
2022-12-15 17:49:09 +0200

    Master

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello People");
-        System.out.println("args = " + args);
+        System.out.println("args =  + args");
         System.out.println("Test number two");
     }
 }
