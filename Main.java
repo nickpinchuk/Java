0ef3450fe8008946240726753a86092b11ef0429
nickpinchuk <nickpinchuk91@gmail.com>
2022-12-16 00:58:30 +0200

    first task commit

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello People");
+        System.out.println("Hello all");
         System.out.println("args =  + args");
         System.out.println("Hello world!");
         System.out.println("args = " + args);
