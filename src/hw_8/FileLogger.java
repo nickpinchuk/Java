84f8ab1bc87d4ebc37e498ee2e7b3a752d81ed37
Nick <nickpinchuk91@gmail.com>
2023-01-18 00:42:59 +0200

    hw8

---
+++ src/hw_8/FileLogger.java
@@ -0,0 +1,35 @@
+package hw_8;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+public class FileLogger extends FileLoggerConfiguration {
+    public static void main(String[] args) {
+        debug("Test");
+        info("Test 2");
+    }
+
+    public static void debug(String line) {
+        try (FileWriter writer = new FileWriter(filePath, true);
+             BufferedWriter BufferedWriter = new BufferedWriter(writer)) {
+            BufferedWriter.write("[" + dat + "]" + "[" + LoggingLevel.INFO + "][" + LoggingLevel.DEBUG + "] Message: " + line + " \n");
+            System.out.println(LoggingLevel.DEBUG + " Successfully written");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void info(String line) {
+        try (FileWriter writer = new FileWriter(filePath, true);
+             BufferedWriter BufferedWriter = new BufferedWriter(writer)) {
+            BufferedWriter.write("[" + dat + "]" + "[" + LoggingLevel.INFO + "] Message: " + line + " \n");
+            System.out.println(LoggingLevel.INFO + " Successfully written");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
