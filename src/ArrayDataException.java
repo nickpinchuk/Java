b158a9dec890383c32a2f9f06161d909f642208f
Nick <nickpinchuk91@gmail.com>
2023-01-19 20:43:33 +0200

    homeworks 8

--- src/ArrayDataException.java
+++
@@ -1,5 +0,0 @@
-public class ArrayDataException extends RuntimeException{
-    public ArrayDataException(int i, int j){
-        super("Incorrect array's data : [" +i+"] [" +j+"]");
-    }
-}
--- src/ArraySizeException.java
+++
@@ -1,6 +0,0 @@
-public class ArraySizeException extends RuntimeException{
-
-    public ArraySizeException() {
-        super("Make array size 4x4");
-    }
-}
